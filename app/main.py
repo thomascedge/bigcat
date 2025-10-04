59b74817d6ac67f5a5d2aa3ddefe0c22d52a8896
Thomas <suavetomas@gmail.com>
2025-10-04 08:24:31 -0500

    cors middleware

--- app/main.py
+++ app/main.py
@@ -2,10 +2,26 @@ import uvicorn
 from fastapi import FastAPI
 from mangum import Mangum
 from app.api import register_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 register_routes(app)
 handler = Mangum(app)
+
+# adding middleware for 
+origins = [
+    "http://localhost:3000",           # dev
+    "https://bigcat.thomascedge.com",  # prod
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def startup():
